6596648a011a568afcdace0381ae15714a99ec2a
Maxime Piraux <maxime.piraux@uclouvain.be>
2019-02-27 11:54:51 +0100

    Ajout d'une action de démarrage dans l'exemple

--- flask_ask_test.py
+++ flask_ask_test.py
@@ -1,13 +1,20 @@
 from flask import Flask
-from flask_ask import Ask, statement
+from flask_ask import Ask, statement, question
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 
+
+@ask.launch
+def start():
+    return question('Quel est votre nom ?')
+
+
 @ask.intent('HelloIntent')
 def hello(firstname):
-    speech_text = "Hello %s" % firstname
-    return statement(speech_text).simple_card('Hello', speech_text)
+    return statement("Bonjour {}".format(firstname))
+
 
 if __name__ == '__main__':
     app.run()
+
